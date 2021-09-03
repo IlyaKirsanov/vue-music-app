21a6f2c05f3821cc1fcf882bb50e23fd1f11f578
Ilya Kirsanov <ilya.kirsanov@dreamhost.com>
2021-09-03 11:35:27 +0300

    Basic app with state

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,15 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    authModalShow: false,
   },
   mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
+  },
+  getters: {
+    // authModalShow: (state) => state.authModalShow,
   },
   actions: {
   },
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/Header.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
+describe('Header.vue', () => {
+  it('renders text Music when passed', () => {
+    const text = 'Music';
     const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+      props: { },
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toMatch(text);
   });
 });
