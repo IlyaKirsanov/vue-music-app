295001f903d57f86ab101728b373122813eef37e
Ilya Kirsanov <ilya.kirsanov@dreamhost.com>
2021-10-07 11:48:53 +0300

    Add props validation

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -11,7 +11,7 @@
           </div>
           <div class="p-6">
             <!-- Composition Items -->
-            <CompositionItem v-for="song in songs" :key="song.docId"/>
+            <CompositionItem v-for="song in songs" :key="song.docId" :song="song"/>
           </div>
         </div>
       </div>
