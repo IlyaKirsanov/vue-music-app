c395caa5c53488f64e141b452985bd357c395bc3
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-11-15 17:26:37 +0200

    Add song item and dynamic route

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -3,13 +3,17 @@
       class="flex justify-between items-center p-3 pl-6 cursor-pointer transition
           duration-300 hover:bg-gray-50">
     <div>
-      <a href="#" class="font-bold block text-gray-600">{{ song.modifiedName }}</a>
+      <router-link
+          :to="{ name:'song', params: { id: song.docId } }"
+          class="font-bold block text-gray-600">
+        {{ song.modifiedName }}
+      </router-link>
       <span class="text-gray-500 text-sm">{{ song.name }}</span>
     </div>
 
     <div class="text-gray-600 text-lg">
       <span class="comments">
-        <i class="fa fa-comments text-gray-600"/>
+        <i class="fa fa-comments text-gray-600" />
         {{ song.commentsCount }}
       </span>
     </div>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Manage from '@/views/Manage.vue';
+import Song from '@/views/Song.vue';
 import store from '@/state/store';
 
 const routes = [
@@ -28,10 +29,11 @@ const routes = [
       requiresAuth: true,
     },
   },
-  // {
-  //   path: '/manage',
-  //   redirect: { name: 'manage' },
-  // },
+  {
+    name: 'song',
+    path: '/song/:id',
+    component: Song,
+  },
   {
     path: '/:catchAll(.*)*',
     redirect: { name: 'home' },
